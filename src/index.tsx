a955cd28314a258220078b7b93ac00b141bdf974
gamangee <gamangee1991@gmail.com>
2023-02-22 17:05:51 +0900

    use styled-components and style reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme } from './theme';
+import { theme } from './styles/theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
